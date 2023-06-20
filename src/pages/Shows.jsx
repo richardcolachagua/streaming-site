ce85411cff83fb54dfcb4247a048709ba2e3c6f9
Richard Colachagua <richardcolachagua@gmail.com>
2023-06-19 23:01:05 -0400

    "Show page commits"

--- src/pages/Shows.jsx
+++ src/pages/Shows.jsx
@@ -1,10 +1,13 @@
-import { Grid } from "@mui/material";
+import { Box, Grid, Link } from "@mui/material";
+import link from "react-router-dom";
 import React from "react";
 
 const Shows = () => {
   return (
-    <Grid>
-      <Grid></Grid>
+    <Grid container spacing={3}>
+      <Grid>
+        <Box sx={{ display: "flex" }}></Box>
+      </Grid>
     </Grid>
   );
 };
