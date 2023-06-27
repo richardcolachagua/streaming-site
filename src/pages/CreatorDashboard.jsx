25c0da23056bcbab6e5f77349c6151599e5e11d3
Richard Colachagua <richardcolachagua@gmail.com>
2023-06-27 15:26:46 -0400

    "Creator Dashboard and Member settings updates"

--- src/pages/CreatorDashboard.jsx
+++ src/pages/CreatorDashboard.jsx
@@ -1,18 +1,23 @@
 import React from "react";
-import { Box, Button, Typography } from "@mui/material";
+import { Box, Button, Typography, Divider, Grid } from "@mui/material";
 
 const CreatorDashboard = () => {
   const uploadButton = () => {};
   return (
-    <Box>
-      <Typography>Channel Dashboard</Typography>
+    <Grid container spacing={3}>
       <Box>
-        <Button variant="contained">Upload Videos</Button>
+        <Typography variant="h1">Channel Dashboard</Typography>
+        <Box>
+          <Button variant="contained">Upload Videos</Button>
+        </Box>
+        <Box>
+          <Typography variant="h2">Channel Analytics</Typography>
+          <Divider variant="middle" />
+
+          <Typography variant="h3">Current Subscribers</Typography>
+        </Box>
       </Box>
-      <Box>
-        <Typography>Channel Analytics</Typography>
-      </Box>
-    </Box>
+    </Grid>
   );
 };
 
