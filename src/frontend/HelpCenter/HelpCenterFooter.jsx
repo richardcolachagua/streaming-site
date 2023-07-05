d09cf119ed9a4f4ccd2561ab5c26d9fa0380fa08
Richard Colachagua <richardcolachagua@gmail.com>
2023-07-04 23:12:54 -0400

    "Help Center and Footer Created"

---
+++ src/frontend/HelpCenter/HelpCenterFooter.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const HelpCenterFooter = () => {
+  return <div>HelpCenterFooter</div>;
+};
+
+export default HelpCenterFooter;
---
+++ src/frontend/HelpCenter/HelpCenterHeader.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const HelpCenterHeader = () => {
+  return <div>HelpCenterHeader</div>;
+};
+
+export default HelpCenterHeader;
