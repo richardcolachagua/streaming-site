2e2cfe3ad1681c756bc3ff13465cef4baf95e73f
Richard Colachagua <richardcolachagua@gmail.com>
2023-07-29 23:34:15 -0400

    "Show Page UI Framework Created"

--- src/frontend/pages/Shows.jsx
+++ src/frontend/pages/Shows.jsx
@@ -1,13 +1,70 @@
-import { Box, Grid, Link, Typography } from "@mui/material";
+import { Box, Grid, Link, Typography, Stack } from "@mui/material";
 import React from "react";
+import Footer from "../components/Footer";
 
 const Shows = () => {
   return (
-    <Grid container spacing={3}>
-      <Grid>
-        <Box sx={{ display: "flex" }}></Box>
-      </Grid>
-    </Grid>
+    <>
+      <Typography variant="h6" sx={{ fontWeight: "bold", textAlign: "left" }}>
+        Continue Watching for ____
+      </Typography>
+      <Stack
+        direction="row"
+        spacing={2}
+        justifyContent="space-between"
+        marginBottom="48px"
+      >
+        <Box
+          sx={{
+            width: 800,
+            height: 150,
+            backgroundColor: "#FF1F33",
+            display: "flex",
+            alignItems: "center",
+            justifyContent: "center",
+          }}
+        >
+          content here
+        </Box>
+        <Box
+          sx={{
+            width: 800,
+            height: 150,
+            backgroundColor: "#FF1F33",
+            display: "flex",
+            alignItems: "center",
+            justifyContent: "center",
+          }}
+        >
+          content here
+        </Box>
+        <Box
+          sx={{
+            width: 700,
+            height: 150,
+            backgroundColor: "#FF1F33",
+            display: "flex",
+            alignItems: "center",
+            justifyContent: "center",
+          }}
+        >
+          content here
+        </Box>
+        <Box
+          sx={{
+            width: 700,
+            height: 150,
+            backgroundColor: "#FF1F33",
+            display: "flex",
+            alignItems: "center",
+            justifyContent: "center",
+          }}
+        >
+          content here
+        </Box>
+      </Stack>
+      <Typography>Top 10 in Shows Today</Typography>
+    </>
   );
 };
 
