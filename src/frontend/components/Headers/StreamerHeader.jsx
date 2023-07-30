fb83e54b4922466e30e22ef6ec340c537aedea04
Richard Colachagua <richardcolachagua@gmail.com>
2023-07-29 23:49:00 -0400

    "Show UI Updated, StreamerHeader Created"

---
+++ src/frontend/components/Headers/StreamerHeader.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const StreamerHeader = () => {
+  return <div>StreamerHeader</div>;
+};
+
+export default StreamerHeader;
--- src/frontend/pages/Shows.jsx
+++ src/frontend/pages/Shows.jsx
@@ -1,10 +1,11 @@
 import { Box, Grid, Link, Typography, Stack } from "@mui/material";
 import React from "react";
 import Footer from "../components/Footer";
-
+import StreamerHeader from "../components/Headers/StreamerHeader";
 const Shows = () => {
   return (
     <>
+      <StreamerHeader />
       <Typography variant="h6" sx={{ fontWeight: "bold", textAlign: "left" }}>
         Continue Watching for ____
       </Typography>
@@ -64,6 +65,132 @@ const Shows = () => {
         </Box>
       </Stack>
       <Typography>Top 10 in Shows Today</Typography>
+      <Stack
+        direction="row"
+        spacing={2}
+        justifyContent="space-between"
+        marginBottom="40px"
+      >
+        <Box
+          sx={{
+            width: 700,
+            height: 150,
+            backgroundColor: "#FF1F33",
+            display: "flex",
+            alignItems: "center",
+            justifyContent: "center",
+          }}
+        >
+          content here
+        </Box>
+        <Box
+          sx={{
+            width: 700,
+            height: 150,
+            backgroundColor: "#FF1F33",
+            display: "flex",
+            alignItems: "center",
+            justifyContent: "center",
+          }}
+        >
+          content here
+        </Box>
+        <Box
+          sx={{
+            width: 700,
+            height: 150,
+            backgroundColor: "#FF1F33",
+            display: "flex",
+            alignItems: "center",
+            justifyContent: "center",
+          }}
+        >
+          content here
+        </Box>{" "}
+        <Box
+          sx={{
+            width: 700,
+            height: 150,
+            backgroundColor: "#FF1F33",
+            display: "flex",
+            alignItems: "center",
+            justifyContent: "center",
+          }}
+        >
+          content here
+        </Box>
+        <Box
+          sx={{
+            width: 700,
+            height: 150,
+            backgroundColor: "#FF1F33",
+            display: "flex",
+            alignItems: "center",
+            justifyContent: "center",
+          }}
+        >
+          content here
+        </Box>
+        <Box
+          sx={{
+            width: 700,
+            height: 150,
+            backgroundColor: "#FF1F33",
+            display: "flex",
+            alignItems: "center",
+            justifyContent: "center",
+          }}
+        >
+          content here
+        </Box>
+      </Stack>
+      <Typography variant="h6" sx={{ fontWeight: "bold", textAlign: "left" }}>
+        Coming Soon
+      </Typography>
+      <Stack
+        direction="row"
+        spacing={2}
+        justifyContent="space-between"
+        marginBottom="40px"
+      >
+        <Box
+          sx={{
+            width: 700,
+            height: 150,
+            backgroundColor: "#FF1F33",
+            display: "flex",
+            alignItems: "center",
+            justifyContent: "center",
+          }}
+        >
+          content here
+        </Box>
+        <Box
+          sx={{
+            width: 700,
+            height: 150,
+            backgroundColor: "#FF1F33",
+            display: "flex",
+            alignItems: "center",
+            justifyContent: "center",
+          }}
+        >
+          content here
+        </Box>
+        <Box
+          sx={{
+            width: 700,
+            height: 150,
+            backgroundColor: "#FF1F33",
+            display: "flex",
+            alignItems: "center",
+            justifyContent: "center",
+          }}
+        >
+          content here
+        </Box>
+      </Stack>
+      <Footer />
     </>
   );
 };
