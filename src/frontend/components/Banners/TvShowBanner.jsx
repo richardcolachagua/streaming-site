0ad4af477e49ad95f62632da103b3099f4b4411e
Richard Colachagua <richardcolachagua@gmail.com>
2023-08-06 23:48:33 -0400

    "Functionalties added to TvShowBanner"

--- src/frontend/components/Banners/TvShowBanner.jsx
+++ src/frontend/components/Banners/TvShowBanner.jsx
@@ -12,6 +12,12 @@ const TvShowBanner = () => {
         setAnchorEl(null);
     }
 
+    const [isMuted, setIsMuted] = useState(true);
+
+    const handleMuteToggle = () => {
+        setIsMuted((prev) => !prev)
+    };
+
     return (
         <Box sx={{ position: "absolute" }}>
             <Typography variant='h4' sx={{ fontWeight: "bold" }}>
@@ -25,12 +31,39 @@ const TvShowBanner = () => {
             >
                 Genres
             </Button>
-            <Button color='white'>
+            <Menu
+                anchorEl={anchorEl}
+                open={open}
+                onClose={handleClose}
+                MenuListProps={{
+                    'aria-labelledby': 'basic-button',
+                }}
+            >
+                <MenuItem onClick={handleClose}>Drama</MenuItem>
+                <MenuItem onClick={handleClose}>Romance</MenuItem>
+                <MenuItem onClick={handleClose}>Comedy</MenuItem>
+                <MenuItem onClick={handleClose}>Sci-Fi</MenuItem>
+                <MenuItem onClick={handleClose}>Animation</MenuItem>
+            </Menu>
+            <Button variant="contained"
+                color="grey"
+                sx={{ borderRadius: 3 }}
+                onClick={handleMuteToggle}>
+                {isMuted ? 'Unmute' : 'Mute'}
+            </Button>
+            <Button variant='contained' color='white' sx={{ borderRadius: 3, }}>
                 Play
             </Button>
-            <Button color='grey'>
+            <Button variant='contained' color='grey' sx={{ borderRadius: 3, }}>
                 More Info
             </Button>
+            <video
+                controls
+                autoPlay
+                muted={isMuted}
+                style={{ width: '100%', height: 'auto' }}
+                src="path/to/video-preview.mp4" // Replace with the actual video URL from the backend
+            />
         </Box>
     )
 }
