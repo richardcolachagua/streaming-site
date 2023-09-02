6b14e7c97ece883583f1adc1199f4f33cd1246b2
Richard Colachagua <richardcolachagua@gmail.com>
2023-09-01 23:50:46 -0400

    "creatordashboard updated"

--- src/frontend/Creator/CreatorDashboard.jsx
+++ src/frontend/Creator/CreatorDashboard.jsx
@@ -1,11 +1,26 @@
 import React, { useState, useEffect } from "react";
 import { Box, Button, Typography, Divider, Grid } from "@mui/material";
 import Header from "../components/Headers/Header";
+import {
+  LineChart,
+  Line,
+  XAxis,
+  YAxis,
+  CartesianGrid,
+  Tooltip,
+  Legend,
+} from "recharts";
 
 const fetchChannelAnalytics = () => {
   const analyticsData = {
     followers: 1000,
     following: 500,
+    analytics: [
+      { month: "Jan", views: 5000 },
+      { month: "Feb", views: 6000 },
+      { month: "Mar", views: 8000 },
+      // Add more data for each month
+    ],
   };
   return analyticsData;
 };
@@ -13,70 +28,129 @@ const fetchChannelAnalytics = () => {
 const CreatorDashboard = () => {
   const [followersCount, setFollowersCount] = useState(0);
   const [followingCount, setFollowingCount] = useState(0);
+  const [analyticsData, setAnalyticsData] = useState([]);
 
   useEffect(() => {
-    const analyticsData = fetchChannelAnalytics();
+    const data = fetchChannelAnalytics();
     setFollowersCount(analyticsData.followers);
     setFollowingCount(analyticsData.following);
+    setAnalyticsData(data.analytics);
   }, []);
 
   return (
     <>
       <Header />
-      <Grid>
-        <Box
-          sx={{
-            width: 300,
-            height: 100,
-            backgroundColor: "primary.dark",
-            "&:hover": {
-              backgroundColor: "primary.main",
-              opacity: [0.9, 0.8, 0.7],
-            },
-            marginLeft: "50px",
-            marginBottom: "50px",
-          }}
-        >
-          <Typography variant="h4" sx={{ fontWeight: "bold" }}>
-            Creator Dashboard
-          </Typography>
-        </Box>
+      <Grid container spacing={3}>
+        <Grid>
+          <Box
+            sx={{
+              width: 300,
+              height: 100,
+              backgroundColor: "primary.dark",
+              "&:hover": {
+                backgroundColor: "primary.main",
+                opacity: [0.9, 0.8, 0.7],
+              },
+              marginLeft: "50px",
+              marginBottom: "50px",
+              display: "flex",
+              alignItems: "center",
+              justifyContent: "center",
+            }}
+          >
+            <Typography variant="h4" sx={{ fontWeight: "bold" }}>
+              Creator Dashboard
+            </Typography>
+          </Box>
+        </Grid>
 
-        <Box
-          sx={{
-            width: 300,
-            height: 300,
-            backgroundColor: "primary.dark",
-            marginLeft: "500px",
-            marginBottom: "50px",
-          }}
-        >
-          <Typography>Upload Your Project Here</Typography>
-          <Button variant="contained" sx={{ fontWeight: "bold" }}>
-            Upload Videos
-          </Button>
-        </Box>
-        <Box
-          sx={{
-            width: 300,
-            height: 300,
-            backgroundColor: "primary.dark",
-            marginLeft: "400px",
-          }}
-        >
-          <Typography variant="h4" sx={{ fontWeight: "bold" }}>
-            Channel Analytics
-          </Typography>
-          <Divider variant="middle" />
+        <Grid item xs={12} sm={6} md={4}>
+          <Box
+            sx={{
+              height: 300,
+              backgroundColor: "primary.dark",
+              display: "flex",
+              flexDirection: "column",
+              alignItems: "center",
+              justifyContent: "center",
+              padding: "24px",
+            }}
+          >
+            <Typography
+              variant="h6"
+              sx={{ fontWeight: "bold", marginBottom: "16px" }}
+            >
+              Upload Your Project Here
+            </Typography>
+            <Button variant="contained" sx={{ fontWeight: "bold" }}>
+              Upload Videos
+            </Button>
+          </Box>
+        </Grid>
 
-          <Typography variant="h4">Current Subscribers</Typography>
-          <Typography variant="h6">Followers: {followersCount}</Typography>
+        <Grid item xs={12} sm={6} md={8}>
+          <Box
+            sx={{
+              height: 300,
+              backgroundColor: "primary.dark",
+              display: "flex",
+              flexDirection: "column",
+              alignItems: "center",
+              justifyContent: "center",
+              padding: "24px",
+            }}
+          >
+            <Typography variant="h4" sx={{ fontWeight: "bold" }}>
+              Channel Analytics
+            </Typography>
+            <Divider variant="middle" sx={{ marginBottom: "16px" }} />
 
-          <Typography variant="h4" sx={{ fontWeight: "bold" }}>
-            Summary
-          </Typography>
-          <Typography variant="h6">Following: {followingCount}</Typography>
-        </Box>
+            <Typography variant="h6" sx={{ marginBottom: "8px" }}>
+              Current Subscribers
+            </Typography>
+            <Typography
+              variant="h4"
+              sx={{ fontWeight: "bold", marginBottom: "16px" }}
+            >
+              Followers: {followersCount}
+            </Typography>
+
+            <Typography variant="h6" sx={{ marginBottom: "8px" }}>
+              Summary
+            </Typography>
+            <Typography
+              variant="h4"
+              sx={{ fontWeight: "bold", marginBottom: "16px" }}
+            >
+              Following: {followingCount}
+            </Typography>
+          </Box>
+        </Grid>
+
+        <Grid item xs={12}>
+          <Box
+            sx={{
+              height: 400,
+              backgroundColor: "primary.dark",
+              padding: "24px",
+            }}
+          >
+            <Typography
+              variant="h4"
+              sx={{ fontWeight: "bold", marginBottom: "14px" }}
+            >
+              Monthly Analytics
+            </Typography>
+            <LineChart width={800} height={300} data={analyticsData}>
+              <CartesianGrid strokeDasharray="3 3" />
+              <XAxis dataKey="month" />
+              <YAxis />
+              <Tooltip />
+              <Legend />
+              <Line type="monotone" dataKey="views" stroke="#8884d8" />
+            </LineChart>
+          </Box>
+        </Grid>
       </Grid>
     </>
   );
